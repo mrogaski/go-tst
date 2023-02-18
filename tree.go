96dfc1a0e68e704e3a94e14cadb11887fb55fbf2
Mark Rogaski <mrogaski@pobox.com>
2023-02-18 09:48:31 -0500

    Added Insert method.

--- tree.go
+++ tree.go
@@ -7,9 +7,56 @@ type ByteSequence interface {
 }
 
 // TernarySearchTree is a structure implementing Sedgewick and Bentley's Ternary Search Tree.
-type TernarySearchTree[K ByteSequence, V any] struct{}
+type TernarySearchTree[K ByteSequence, V any] struct {
+	root *node[V]
+}
+
+type node[V any] struct {
+	splitChar byte
+	loKid     *node[V]
+	eqKid     *node[V]
+	hiKid     *node[V]
+}
 
 // New returns an initialized, empty search tree.
+// K is a string or byte slice type for the keys and V is any type for the values.
 func New[K ByteSequence, V any]() *TernarySearchTree[K, V] {
 	return new(TernarySearchTree[K, V])
+}
+
+// Insert adds a value to the tree, returning true if the value is updating a previous value or false if this
+// is the first value inserted with the key.
+func (t *TernarySearchTree[K, V]) Insert(key K, value V) bool {
+	sequence := []byte(key)
+
+	nodePtr := &t.root
+
+	updated := true
+
+	i := 0
+
+	for {
+		if i >= len(sequence) {
+			break
+		}
+
+		char := sequence[i]
+
+		if *nodePtr == nil {
+			*nodePtr = &node[V]{splitChar: char}
+			updated = false
+		}
+
+		switch {
+		case char < (*nodePtr).splitChar:
+			nodePtr = &(*nodePtr).loKid
+		case char > (*nodePtr).splitChar:
+			nodePtr = &(*nodePtr).hiKid
+		default:
+			nodePtr = &(*nodePtr).eqKid
+			i++
+		}
+	}
+
+	return updated
 }
--- tree_test.go
+++ tree_test.go
@@ -16,3 +16,65 @@ func TestNew(t *testing.T) {
 	assert.IsType(t, &tst.TernarySearchTree[string, int]{}, got)
 	assert.NotNil(t, got)
 }
+
+func TestTernarySearchTree_Insert(t *testing.T) {
+	t.Parallel()
+
+	type element struct {
+		k string
+		v int
+	}
+
+	tests := []struct {
+		name    string
+		preload []element
+		key     string
+		value   int
+		want    bool
+	}{
+		{
+			name:  "initital",
+			key:   "firewater",
+			value: 1,
+			want:  false,
+		},
+		{
+			name: "new",
+			preload: []element{
+				{k: "fire", v: 1},
+			},
+			key:   "firewater",
+			value: 2,
+			want:  false,
+		},
+		{
+			name: "update",
+			preload: []element{
+				{k: "firewater", v: 1},
+			},
+			key:   "firewater",
+			value: 2,
+			want:  true,
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			tree := tst.New[string, int]()
+
+			if tc.preload != nil {
+				for _, elem := range tc.preload {
+					tree.Insert(elem.k, elem.v)
+				}
+			}
+
+			got := tree.Insert(tc.key, tc.value)
+
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
